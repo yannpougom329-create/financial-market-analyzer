9a55bb97218c20bd3e016381ee471a630abf24f9
yannpougom329-create <yannpougom329@gmail.com>
2026-09-07 14:22:43 +0100

    correction des calculs et graphiques

--- main.py
+++ main.py
@@ -13,31 +13,34 @@ def backtest(ticker):
             print(ma_courte,ma_longue)
             data["MA_courte"] = data["Close"].rolling(window=ma_courte).mean() 
             data["MA_longue"] = data["Close"].rolling(window=ma_longue).mean() 
-    data["signal"]= 0
-    data.loc[data["MA_courte"] > data["MA_longue"], "signal"] = 1
-    data["position"] = data["signal"].diff()
-    frais = 0.001
-    data["frais"] = 0.0
-    data.loc[data["position"] != 0, "frais"]= frais
-    data["Retour"] = data["Close"].pct_change()
-    data["Retour_strategie"] = data["Retour"] * data["signal"].shift(1)
-    data["Retour_strategie_net"] = data["Retour_strategie"] - data["frais"]
-    data["performance_buyhold"] = (1 + data["Retour"]).cumprod()
-    performance_buyhold = data["performance_buyhold"].iloc[-1]
-    performance_strategie = (1 + data["Retour_strategie"]).cumprod().iloc[-1]
-    performance_strategie_net= (1 + data["Retour_strategie_net"]).cumprod().iloc[-1]
-    resultats.append({
-          "MA_courte" : ma_courte,
-          "MA_longue" : ma_longue,
-          "Performance" : performance_strategie_net
-    })
+            data["signal"]= 0
+            data.loc[data["MA_courte"] > data["MA_longue"], "signal"] = 1
+            data["position"] = data["signal"].diff()
+            frais = 0.001
+            data["frais"] = 0.0
+            data.loc[data["position"].notna() & ( data["position"] != 0), "frais"]= frais
+            data["Retour"] = data["Close"].pct_change()
+            data["Retour_strategie"] = data["Retour"] * data["signal"].shift(1)
+            data_test = data.dropna(
+                   subset=["MA_courte", "MA_longue", "Retour", "Retour_strategie"]
+            )
+            data_test["Retour_strategie_net"] = data_test["Retour_strategie"] - data_test["frais"]
+            data_test["performance_buyhold"] = (1 + data_test["Retour"]).cumprod()
+            performance_buyhold = data_test["performance_buyhold"].iloc[-1]
+            performance_strategie = (1 + data_test["Retour_strategie"]).cumprod().iloc[-1]
+            performance_strategie_net= (1 + data_test["Retour_strategie_net"]).cumprod().iloc[-1]
+            resultats.append({
+               "MA_courte" : ma_courte,
+               "MA_longue" : ma_longue,
+               "Performance" : performance_strategie_net
+            })
+            if performance_strategie_net > meilleure_performance:
+                           meilleure_performance = performance_strategie_net
+                           meilleure_ma_courte = ma_courte
+                           meilleure_ma_longue = ma_longue
     resultats = pd.DataFrame(resultats)
     resultats = resultats.sort_values("Performance", ascending=False)
     print(resultats.head(10))
-    if performance_strategie_net > meilleure_performance:
-               meilleure_performance = performance_strategie_net
-               meilleure_ma_courte = ma_courte
-               meilleure_ma_longue = ma_longue
     print("meilleur moyenne courte:", meilleure_ma_courte)
     print("meilleur moyenne longue:", meilleure_ma_longue)
     print("meilleur performance", meilleure_performance)
@@ -50,13 +53,13 @@ def backtest(ticker):
     data["capital_strategie"] = capital_initial *(1 + data["Retour_strategie"]).cumprod()
     print(data[["capital_buyhold", "capital_strategie"]].tail(10))
     nbre_jours = len(data)
-    performance_annuelle = ((1 + data["Retour_strategie"].cumprod().iloc[-1])** (252 / nbre_jours) -1)
+    performance_annuelle = (performance_strategie_net ** (252 / nbre_jours)) -1
     print(f"performance annuelle : {performance_annuelle*100:.2f}%")
-    volatilité = data["Retour_strategie"].std() * (252 ** 0.5)
+    volatilité = data_test["Retour_strategie_net"].std() * (252 ** 0.5)
     print(f"Volatilité : {volatilité*100:.2f}%")
     sharpe = performance_annuelle / volatilité
     print(f"sharpe ratio : {sharpe:.2f}")
-    capital = (1 + data["Retour_strategie"]).cumprod()
+    capital = (1 + data_test["Retour_strategie_net"]).cumprod()
     plus_haut =capital.cummax()
     drawdown = (capital - plus_haut) / plus_haut
     drawdown_max = drawdown.min()
@@ -64,8 +67,8 @@ def backtest(ticker):
     print(data.tail(20))
     plt.figure(figsize=(12,6))
     plt.plot(data.index, data["Close"], label="prix")
-    plt.plot(data.index, data["MA_courte"], label=["MA_courte"])
-    plt.plot(data.index, data["MA_longue"], label=["MA_longue"])
+    plt.plot(data.index, data["MA_courte"], label="MA_courte")
+    plt.plot(data.index, data["MA_longue"], label="MA_longue")
     plt.title("Prix de l'action")
     plt.xlabel("date")
     plt.ylabel("prix ($)")
